e40049652341ad62a7b691671b210d95bd0a9781
your_github_username <valentinaleonh8@gmail.com>
2025-01-09 23:40:58 -0600

    Start working on 'add_song' method

--- linked_lists/music_playlist_manager/playlist.rb
+++ linked_lists/music_playlist_manager/playlist.rb
@@ -9,7 +9,20 @@ class Playlist
         @current_song = nil
     end
 
-    def add_song
+    def add_song(title, artist, duration)
+        # Create new song with title, artist and duration
+        song = Song.new(title, artist, duration)
+        if @head.nil?
+            @head = song 
+            @tail = song
+            @current_song = song 
+        else 
+            # add the song at the end
+            @tail.next = song 
+            song.prev = @tail 
+            @tail = song 
+        end 
+        puts "Song '#{title}' added to the playlist!"
     end
 
     def remove_song(title)
